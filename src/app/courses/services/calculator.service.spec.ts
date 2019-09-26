92a5d20ab026f3c1d3e53368194c8dbdb17d87ca
Roden, Lee <36708@nelnet.net>
2019-09-26 16:31:38 -0500

    Update Calculator Test

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,53 +1,61 @@
-import {CalculatorService} from './calculator.service';
-import {LoggerService} from './logger.service';
-import {TestBed} from '@angular/core/testing';
+import { createServiceFactory, SpectatorService, SpyObject } from '@ngneat/spectator';
+import { CalculatorService } from './calculator.service';
+import { LoggerService } from './logger.service';
+import { TestBed } from '@angular/core/testing';
 
+describe ('CalculatorService', () => {
+    let calculator: CalculatorService;
+    let loggerSpy: any;
 
-describe('CalculatorService', () => {
-
-    let calculator: CalculatorService,
-        loggerSpy: any;
-
-    beforeEach(()=> {
-
-        console.log("Calling beforeEach");
-
-        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+    beforeEach(function() {
+        loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
 
         TestBed.configureTestingModule({
             providers: [
                 CalculatorService,
-                {provide: LoggerService, useValue: loggerSpy}
+                { provide: LoggerService, useValue: loggerSpy }
             ]
         });
-
         calculator = TestBed.get(CalculatorService);
-
     });
 
     it('should add two numbers', () => {
-
-        console.log("add test");
-
         const result = calculator.add(2, 2);
 
         expect(result).toBe(4);
-
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-
     });
-
 
     it('should subtract two numbers', () => {
-
-        console.log("subtract test");
-
         const result = calculator.subtract(2, 2);
 
-        expect(result).toBe(0, "unexpected subtraction result");
-
+        expect(result).toBe(0, 'unexpected subtraction result');
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-
     });
 
+});
+
+describe ('CalculatorService using Spectator', () => {
+    let spectator: SpectatorService<CalculatorService>;
+    let loggerService: SpyObject<LoggerService>;
+
+    const createService = createServiceFactory({
+        service: CalculatorService,
+        mocks: [LoggerService]
+    });
+
+    beforeEach(() => {
+        spectator = createService();
+        loggerService = TestBed.get(LoggerService);
+    });
+
+    it('should add two numbers', () => {
+      expect(spectator.service.add(2, 2)).toBe(4);
+      expect(loggerService.log).toHaveBeenCalledTimes(1);
+    });
+
+    it('should subtract two numbers', () => {
+        expect(spectator.service.subtract(2, 2)).toBe(0, 'unexpected subtraction result');
+        expect(loggerService.log).toHaveBeenCalledTimes(1);
+    });
 });
